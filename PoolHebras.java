72ad8de3ee0db3d5ed7418d89104b5b61372dfab
aruzrojas <49320316+aruzrojas@users.noreply.github.com>
2019-04-12 20:11:21 -0400

    Update PoolHebras.java

--- PoolHebras.java
+++ PoolHebras.java
@@ -1,5 +1,3 @@
-// clase que crea la lista de hebras
-
 import java.util.Scanner;
 import java.io.PrintStream;
 import java.io.InputStream;
@@ -17,14 +15,14 @@ import java.net.Socket;
 public class PoolHebras{
     private final int nHebras;
     private final ListaHebras[] Hebras;
-    private final LinkedBlockingQueue Cola;
+    private final LinkedBlockingQueue<Runnable> Cola;
 
     public PoolHebras(int nHebras){
         this.nHebras = nHebras;
-        Cola = new LinkedBlockingQueue();
+        Cola = new LinkedBlockingQueue<Runnable>();
         Hebras = new ListaHebras[nHebras];
         
-        for (int i=0; i < nHebras; i++){
+        for (int i=0; i < this.nHebras; i++){
             Hebras[i] = new ListaHebras();
             Hebras[i].start();  
         }
@@ -35,7 +33,7 @@ public class PoolHebras{
             Cola.notify();
         }
     }
-    private class ListaHebras extends Thread{
+    public class ListaHebras extends Thread{
         public void run(){
             Runnable proceso;
             while(true){
@@ -48,6 +46,17 @@ public class PoolHebras{
                             System.out.println("Ocurrio un error en la espera de la cola "+ e.getMessage());
                         }
                     }
+                    proceso = Cola.poll();
+                }
+                try{
+                    proceso.run();
+                } catch(RuntimeException e){
+                    System.out.println("La ThreadPool ha sido interrumpida"+ e.getMessage());
+                }
+            }
+        }
+    }
+}
                     proceso = (Runnable)Cola.poll();
                 }
                 try{
