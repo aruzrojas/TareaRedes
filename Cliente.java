0156b6f79d6aff3f49b21f37ec84a230f6287656
aruzrojas <49320316+aruzrojas@users.noreply.github.com>
2019-05-16 22:21:50 -0300

    while x for

--- Cliente.java
+++ Cliente.java
@@ -1,18 +1,16 @@
 // entrada y salida
 import java.util.Scanner;
 import java.io.PrintStream;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.io.FileOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+// archivos
 // excepciones
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.IOException;
-// sockets y hebras
-import java.net.ServerSocket;
+// hebras y sockets
 import java.net.Socket;
-import java.lang.Thread;
 public class Cliente {
     public static void main(String[] args) throws IOException {
         String mensaje, mensajeterminal;
@@ -24,12 +22,7 @@ public class Cliente {
         Scanner entradaDatos = new Scanner(socket.getInputStream()); // entrada
         PrintStream salidaDatos = new PrintStream(socket.getOutputStream()); // salida
         // para los archivos
-        InputStream in = null;
-        OutputStream out = null;
-        // abro el archivo
-        File file = null;
-
-        byte[] bytes = new byte[16*1024];
+        FileOutputStream fos = null;
 
         // recibo un mensaje
         mensaje = entradaDatos.nextLine();
@@ -40,41 +33,97 @@ public class Cliente {
         inputterminal = new Scanner(System.in);
         // paso de mensajes
         while(true){
+            System.out.print("> ");
             mensajeterminal = inputterminal.nextLine();
 
-            salidaDatos.println(mensajeterminal);
             // veo como tratar la respuesta al comando
+            // EXIT
             if (mensajeterminal.equals("Exit")) {
+                salidaDatos.println(mensajeterminal);
                 break;
             }
-            else if(mensaje.matches("^get [a-zA-Z0-9]*\\.[a-zA-Z0-9]*$")){
-                mensaje = mensaje.substring(4);
+            // LS
+            else if(mensajeterminal.equals("ls")){
+                salidaDatos.println(mensajeterminal);
 
-                // recibo el archivo
-                in = socket.getInputStream();
+                int largo;
                 try {
-                    out = new FileOutputStream(mensaje);
-                } catch (FileNotFoundException e) {
-                    try {
-                        file = new File(mensaje);
-                        file.createNewFile();
-                        out = new FileOutputStream(mensaje);
-                    } catch (Exception er) {
-                        System.err.println("Error al crear el archivo");
-                    }
+                    largo = Integer.parseInt(entradaDatos.nextLine()); 
+                } catch (Exception e) {
+                    System.out.println("Error al obtener el valor");
+                    largo = 0;
                 }
 
-                int count;
-                while((count = in.read(bytes)) > 0){
-                    out.write(bytes,0,count);
+                for (int i = 0; i < largo; i++) {
+                    mensaje = entradaDatos.nextLine();
+                    System.out.println(mensaje);
                 }
-                in.close();
-                out.close();
-
-                mensaje = entradaDatos.nextLine();
-                System.out.println(mensaje);
             }
+            // GET
+            else if(mensajeterminal.matches("^get [a-zA-Z0-9]+(\\.[a-zA-Z0-9]+)*$")){
+                salidaDatos.println(mensajeterminal);
+                String archivo = mensajeterminal.substring(4);
+                int bytesread;
+
+                DataInputStream entradad = new DataInputStream(socket.getInputStream());
+                fos = new FileOutputStream(archivo);
+                long tamanio = entradad.readLong();
+                System.out.print("Tamaño archivo: ");
+                System.out.println(tamanio);
+            
+                byte[] buffer = new byte[1024];
+                while (tamanio > 0 && (bytesread = entradad.read(buffer, 0, (int)Math.min(buffer.length, tamanio))) != -1) {
+                    fos.write(buffer, 0, bytesread);
+                    tamanio -= bytesread;
+                    System.out.print("Queda: ");
+                    System.out.println(tamanio);
+                }
+                fos.close();
+                //System.out.println("Hola");
+            }
+            // PUT
+            else if(mensajeterminal.matches("^put [a-zA-Z0-9]+(\\.[a-zA-Z0-9]+)*$")){
+                String archivo = mensajeterminal.substring(4);
+                
+                File file = new File(archivo);
+                // se checkea que el archivo existe
+                if(file.exists()){ // el archivo existe
+                    // envio el mensaje
+                    salidaDatos.println(mensajeterminal);
+                    try {
+                        // variables a usar
+                        byte[] bytearray = new byte[(int)file.length()];
+                        // guardo el archivo en un arreglo
+                        DataInputStream bis = new DataInputStream(new FileInputStream(archivo));
+                        bis.readFully(bytearray, 0, bytearray.length);
+                        // imprimo por pantalla el largo de este
+                        System.out.print("Tamaño archivo: ");
+                        System.out.println(bytearray.length);
+                        // envio los datos
+                        DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+
+                        // envio el largo del archivo
+                        salidaDatos.println(bytearray.length);
+                        salidaDatos.flush();
+                        // envio el archivo
+                        dos.write(bytearray, 0, bytearray.length);
+                        
+                        dos.flush();
+                        // cierro lo que no necesito
+                        bis.close();
+                        // termino de enviar el archivo
+                    } catch (Exception e) {
+                        System.err.println("Error en el envio del archivo");
+                        salidaDatos.println("Error al enviar el archivo " + mensaje);
+                    }
+                } else {
+                    System.out.println("Error: el archivo no existe");
+                }
+
+            }
+            // CUALQUIERA, incluido el DELETE
             else{
+                salidaDatos.println(mensajeterminal);
                 mensaje = entradaDatos.nextLine();
                 System.out.println(mensaje);
             }
@@ -83,19 +132,7 @@ public class Cliente {
         System.out.println("Fin de la conexión");
         inputterminal.close();
         entradaDatos.close();
+        salidaDatos.close();
         socket.close();
-    }
-}
-        // out = cs.getOutputStream();
-
-        // int count;
-        // while((count = in.read(bytes)) > 0){
-        //     out.write(bytes, 0, count);
-        // }
-        
-        System.out.println("Fin de la conexión");
-        inputterminal.close();
-        entradaDatos.close();
-        cs.close();
     }
 }
